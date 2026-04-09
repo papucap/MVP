3f5a23947bba29f9b36619f45affdc6d171b99a9
papuca <papuca@PC1116>
2026-04-09 10:35:40 +0200

    Logger + Room

---
+++ MVP/Logger.cs
@@ -0,0 +1,10 @@
+﻿public class Logger
+{
+    private string path = "logs/server.log";
+
+    public void Log(string message)
+    {
+        var line = $"[{DateTime.Now}] {message}";
+        File.AppendAllText(path, line + "\n");
+    }
+}
---
+++ MVP/Room.cs
@@ -0,0 +1,10 @@
+﻿public class Room
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public List<string> Exits { get; set; } = new();
+    public List<Item> Items { get; set; } = new();
+    public List<NPC> NPCs { get; set; } = new();
+}
